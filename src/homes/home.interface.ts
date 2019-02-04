628e310adeae5a8812f86bf8d4cf742fba676160
jim.arbete <jim.arbete@gmail.com>
2019-02-05 00:51:33 +0100

    ?

--- src/homes/home.interface.ts
+++ src/homes/home.interface.ts
@@ -2,19 +2,4 @@ export class Home {
     readonly id: number
     readonly name: string
     readonly rooms: any
-
-    // id: '1',
-    // name: 'Hus1',
-    // rooms: [
-    //   {
-    //     name: 'Vardagsrum',
-    //     temperature: 21,
-    //     humidity: 0.2
-    //   },
-    //   {
-    //     name: 'Kök',
-    //     temperature: 28,
-    //     humidity: 0.95
-
-    //   }
 }
